4e715dcaf14cefc094612aa339ce006cdfc18212
Gerald Vasquez Aleman <95062993+gvaleman@users.noreply.github.com>
2025-08-07 09:15:25 -0600

    Update mis_funciones.R

--- mis_funciones.R
+++ mis_funciones.R
@@ -685,9 +685,4 @@ contar <- function(data, columna = NULL) {
 set_my_dir <- function() {
   setwd(rstudioapi::getActiveDocumentContext()$path %>% dirname())
   cat("El directorio fue configurado en:", getwd(), "\n")
-} 
-
- #-------------------------------------#
-#     Alias para el pipe      #
-#-------------------------------------#                           
-`%p%` <- magrittr::`%>%`                          
+}                  
